84460abcf445d5b1b957e2efad77b808a37068c7
NoWaaLT <vytautas.jelisejevas@gmail.com>
2021-10-05 22:44:53 +0300

    Til

--- Biudzetas/src/Main.java
+++ Biudzetas/src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int option; // main programos meniu
+//        String option; // main programos meniu
         int pajamuIrasasCounter = 0;
         int islaiduIrasasCounter = 0;
         int iBanka = 0; // pozymis ar i banko saskaita
@@ -13,8 +13,7 @@ public class Main {
         int islaidosGrynais = 0;
         int islaidosKortele = 0;
         boolean onOrOff = true;
-        boolean onOrOff2 = true;
-        boolean YN = true; // pozymis y ar n
+        boolean YN; // pozymis y ar n
         String pozymisArIBanka = null;
         String pozymisArIBanka2 = null;
         Biudzetas b1 = new Biudzetas();
@@ -22,35 +21,38 @@ public class Main {
         int option3; // islaidu antram switchui
         int indeksoNr; // user nurodyti iraso indeksui laikyti
         LocalDateTime dateTime = LocalDateTime.now();
+        String option;
 
         System.out.println("Programa - biudzetas!");
 
         Scanner scn = new Scanner(System.in);
 
-        while (onOrOff == true) {
+        while (onOrOff) {
 
-            System.out.println("1. Iveskite pajamas \n"
-                    + "2. Iveskite islaidas \n" +
+            System.out.println("1. Iveskite pajamas \n" +
+                    "2. Iveskite islaidas \n" +
                     "3. Ispausdinti pajamu irasus \n" +
                     "4. Ispausdinti islaidu irasus \n" +
                     "5. Isjungti programa");
 
-            option = scn.nextInt();
+            option = scn.next();
+
             switch (option) {
-                case 1:
+                case "1":
+                    scn.nextLine();
                     System.out.println("Iveskite pajamas : ");
-                    String suma = scn.next();
+                    String suma = scn.nextLine();
                     String dataIrLaikas = dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
                     System.out.println("Pajamu ivedimo data ir laikas " + dataIrLaikas);
                     String data = dataIrLaikas;
                     System.out.println("Pajamu kategorija : ");
-                    String kategorija = scn.next();
+                    String kategorija = scn.nextLine();
                     YN = true;
 
-                    while (YN == true) {
+                    while (YN) {
                         System.out.println("Pozymis ar i banka gauti pinigai? \n" +
                                 "TAIP/NE?");
-                        pozymisArIBanka = scn.next();
+                        pozymisArIBanka = scn.nextLine();
                         if (pozymisArIBanka.equalsIgnoreCase("TAIP")) {
                             iBanka++;
                             YN = false;
@@ -62,27 +64,27 @@ public class Main {
                         }
                     }
                     System.out.println("Iveskite papildoma informacija : ");
-                    String papildomaInfo = scn.next();
+                    String papildomaInfo = scn.nextLine();
                     PajamuIrasas p1 = new PajamuIrasas(suma, data, kategorija, pozymisArIBanka, papildomaInfo);
 
                     b1.setInfo(p1);
                     pajamuIrasasCounter++;
                     break;
 
-                case 2:
+                case "2":
                     System.out.println("Iveskite islaidas : ");
-                    String suma2 = scn.next();
+                    String suma2 = scn.nextLine();
                     String dataIrLaikas2 = dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
                     System.out.println("Islaidu ivedimo data ir laikas " + dataIrLaikas2);
                     String data2 = dataIrLaikas2;
                     System.out.println("Islaidu kategorija : ");
-                    String kategorija2 = scn.next();
+                    String kategorija2 = scn.nextLine();
                     YN = true;
 
-                    while (YN == true) {
+                    while (YN) {
                         System.out.println("Atsiskaitymo budas :? \n" +
                                 "GRYNAIS/KORTELE?");
-                        pozymisArIBanka2 = scn.next();
+                        pozymisArIBanka2 = scn.nextLine();
                         if (pozymisArIBanka2.equalsIgnoreCase("GRYNAIS")) {
                             islaidosGrynais++;
                             YN = false;
@@ -94,14 +96,13 @@ public class Main {
                         }
                     }
                     System.out.println("Iveskite papildoma informacija : ");
-                    String papildomaInfo2 = scn.next();
+                    String papildomaInfo2 = scn.nextLine();
                     IslaiduIrasas i1 = new IslaiduIrasas(suma2, data2, kategorija2, pozymisArIBanka2, papildomaInfo2);
 
                     b1.setInfo(i1);
                     islaiduIrasasCounter++;
                     break;
-
-                case 3:
+                case "3":
                     System.out.println("Is viso yra " + pajamuIrasasCounter + " irasu.");
                     System.out.println("1. Spausdinti viena irasa : \n" +
                             "2. Spausdinti visus pajamu irasus");
@@ -112,19 +113,17 @@ public class Main {
                             System.out.println("Nurodykite indeksa,tarp 0 ir " + (pajamuIrasasCounter - 1) + ", kuri irasa noresite atspausdinti :");
                             indeksoNr = scn.nextInt();
                             b1.getPajamosSingleInfo(indeksoNr);
-                            onOrOff2 = false;
                             break;
                         case 2:
                             System.out.println("Programa spaudina visus irasus : ");
                             b1.getPajamosInfoAll();
-                            onOrOff2 = false;
                             break;
                         default:
                             System.out.println("Tokio pasirinkimo nera.");
                     }
 
                     break;
-                case 4:
+                case "4":
                     System.out.println("Is viso yra " + islaiduIrasasCounter + " irasu.");
                     System.out.println("1. Spausdinti viena irasa : \n" +
                             "2. Spausdinti visus islaidu irasus");
@@ -134,26 +133,27 @@ public class Main {
                             System.out.println("Nurodykite indeksa,tarp 0 ir " + (islaiduIrasasCounter - 1) + ", kuri irasa noresite atspausdinti :");
                             indeksoNr = scn.nextInt();
                             b1.getIslaidosSingleInfo(indeksoNr);
-                            onOrOff2 = false;
                             break;
                         case 2:
                             System.out.println("Programa spaudina visus irasus : ");
                             b1.getIslaidosInfoAll();
-                            onOrOff2 = false;
                             break;
                         default:
                             System.out.println("Tokio pasirinkimo nera.");
                     }
+                    break;
 
-                case 5:
+                case "5":
                     onOrOff = false;
                 default:
-                    if (option != 1 | option != 2 | option != 3 | option != 4 | option != 5) {
-                        System.out.println("Klaida! Tokios pasirinkties nera. Griztama i pagridini langa");
+                    if (option.equals("5")) {
+                        System.out.println("Programa baigia darba.");
+                        break;
                     }
-
-                    System.out.println("--------------------------------");
-                    break;
+//                    else {
+//                        System.out.println("Klaida! Tokio pasirinkimo nera.");
+//                        continue;
+//                    }
             }
         }
 
